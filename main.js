b818033f37390868ac67484793dbedd8319f5da1
gaianacheli <gaianacheli04@gmail.com>
2021-10-11 14:38:54 +0200

    richiesti i km e l'età

---
+++ main.js
@@ -0,0 +1,10 @@
+//chiedi quanti km vuole percorrere
+
+const numeroKm = prompt ('quanti chilometri vuoi percorrere?')
+console.log (numeroKm)
+
+
+//chiedi quanti anni ha
+
+const numberAge = prompt ('quanti anni hai?')
+console.log (numberAge)
