32db3007293071240a82a27cb2f557c6c9d248be
gaianacheli <gaianacheli04@gmail.com>
2021-10-11 15:21:58 +0200

    esercizio completo

--- main.js
+++ main.js
@@ -1,10 +1,24 @@
 //chiedi quanti km vuole percorrere
 
 const numeroKm = prompt ('quanti chilometri vuoi percorrere?')
-console.log (numeroKm)
+console.log ('I Km che vuole percorrere sono ' + numeroKm)
 
 
 //chiedi quanti anni ha
 
 const numberAge = prompt ('quanti anni hai?')
-console.log (numberAge)
+console.log ('gli anni che ha sono ' +  numberAge)
+
+
+//prezzo biglietto
+var prezzoBiglietto = 0.21 * numeroKm
+
+//condizioni
+
+
+if (numberAge < 18) {
+   prezzoBiglietto = prezzoBiglietto * 0.8 
+}else if (numberAge > 65) {
+   prezzoBiglietto = prezzoBiglietto * 0.6
+}
+console.log ('Il prezzo del biglietto è ' + prezzoBiglietto + ' €')
